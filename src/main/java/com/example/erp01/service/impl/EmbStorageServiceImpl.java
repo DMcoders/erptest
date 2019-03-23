81b0f175b1e6b43e3439cbefbdf5ccf83669b8ae
DMcoders <hengzhang64@gmail.com>
2019-03-23 22:30:26 +0800

    测试

--- src/main/java/com/example/erp01/service/impl/EmbStorageServiceImpl.java
+++ src/main/java/com/example/erp01/service/impl/EmbStorageServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.erp01.mapper.EmbStorageMapper;
 import com.example.erp01.model.EmbStorage;
 import com.example.erp01.service.EmbStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmbStorageServiceImpl implements EmbStorageService {
     @Autowired
     private EmbStorageMapper embStorageMapper;
